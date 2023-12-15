85139c0964a1d5e96d194dd8837be2e0f305322d
Francisco Arantes <Francisco.arantes.16864@gmail.com>
2023-12-15 21:54:55 +0000

    CRUD done

--- Services/DeviceWS.asmx.cs
+++ Services/DeviceWS.asmx.cs
@@ -50,61 +50,40 @@ namespace DeviceService.Services
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="device"></param>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="state"></param>
+        /// <param name="value"></param>
+        /// <param name="houseId"></param>
         /// <returns></returns>
-        public int InsertDevice(Device device)
+        public int InsertDevice(int id, string name, bool state, double value, int houseId)
         {
-            int tot = 0;
-            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DevicesConnectionString"].ConnectionString))
-            {
-                con.Open();
-                string query = @"INSERT INTO Devices(Name, State, Value, HouseId) 
-                                VALUES(@Name, @State, @Value, @HouseId)";
-
-                SqlCommand cmd = new SqlCommand(query, con);
-
-                cmd.Parameters.Add("@Name", SqlDbType.NChar).Value = device.Name;
-                cmd.Parameters.Add("@State", SqlDbType.Bit).Value = device.State;
-                cmd.Parameters.Add("@Value", SqlDbType.Float).Value = device.Value;
-                cmd.Parameters.Add("@HouseId", SqlDbType.Int).Value = device.HouseId;
-
-                tot = cmd.ExecuteNonQuery();
-
-                con.Close();
-            }
-            return tot;
-        }
-
-        [WebMethod(Description = "")]
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="device"></param>
-        /// <returns></returns>
-        public int UpdateDevice(Device device)
-        {
+            int rowsAffected = 0;
             try
             {
-                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DevicesConnectionString"].ConnectionString);
-                
-                string cs = "UPDATE Devices SET State='" + 
-                    device.State + "'WHERE Name='" + 
-                    device.Name + "';";
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DevicesConnectionString"].ConnectionString))
+                {
+                    con.Open();
 
-                SqlCommand cmd = con.CreateCommand();
+                    // parameterized queries for better security and to prevent SQL injection.
+                    string query = "INSERT INTO Devices(Id, Name, State, Value, HouseId) " +
+                                   "VALUES(@Id, @Name, @State, @Value, @HouseId)";
 
-                SqlDataAdapter da = new SqlDataAdapter();
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", id);
+                        cmd.Parameters.AddWithValue("@Name", name);
+                        cmd.Parameters.AddWithValue("@State", state);
+                        cmd.Parameters.AddWithValue("@Value", value);
+                        cmd.Parameters.AddWithValue("@HouseId", houseId);
 
-                da.SelectCommand = cmd;
-
-                DataSet ds = new DataSet();
-
-                da.Fill(ds, "All");
-                return 1;
+                        rowsAffected = cmd.ExecuteNonQuery();
+                    }
+                }
+                return rowsAffected;
             }
             catch (Exception e)
             {
-
                 throw e;
             }
         }
@@ -113,33 +92,69 @@ namespace DeviceService.Services
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="device"></param>
+        /// <param name="name"></param>
+        /// <param name="state"></param>
         /// <returns></returns>
-        public int DeleteDevice(Device device)
+        public int UpdateDevice(string name, bool state)
         {
+            int rowsAffected = 0;
             try
             {
-                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DevicesConnectionString"].ConnectionString);
-                string commandString = "DELETE * FROM Devices WHERE State='" + 
-                    device.State + "';";
-                
-                SqlCommand command = con.CreateCommand();
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DevicesConnectionString"].ConnectionString))
+                {
+                    con.Open();
 
-                command.CommandText = commandString;
+                    //parameterized queries for better security and to prevent SQL injection.
+                    string cs = "UPDATE Devices SET State=@State WHERE Name=@Name";
 
-                SqlDataAdapter da = new SqlDataAdapter();
+                    SqlCommand cmd = new SqlCommand(cs, con);
+                    cmd.Parameters.AddWithValue("@State", state);
+                    cmd.Parameters.AddWithValue("@Name", name);
 
-                da.SelectCommand = command;
+                    rowsAffected = cmd.ExecuteNonQuery();
 
-                DataSet ds = new DataSet();
-
-                da.Fill(ds, "All");
-                return 1;
+                    con.Close();
+                }
+                return rowsAffected;
             }
             catch (Exception e)
             {
                 throw e;
             }
         }
+
+        [WebMethod(Description = "")]
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public int DeleteDevice(bool state)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DevicesConnectionString"].ConnectionString))
+                {
+                    con.Open();
+
+                    // Use parameterized query to prevent SQL injection
+                    string commandString = "DELETE FROM Devices WHERE State = @State";
+
+                    using (SqlCommand command = new SqlCommand(commandString, con))
+                    {
+                        command.Parameters.AddWithValue("@State", state);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        return rowsAffected;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
     }
 }
